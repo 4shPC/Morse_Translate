0e0d1569c41dbd996e380ed8f3d6e28dcbfd00d4
GoodGuyAsh <67311318+GoodGuyAsh@users.noreply.github.com>
2021-02-17 10:48:38 +0530

    Made it into module :)

--- morse_.py
+++ morse_.py
@@ -1,17 +1,20 @@
 import csv
 
-s = input('Your Input: ')
-s.lower()
-morse = []
-for i in s:
-    if i.isalnum():
-        with open('morse.csv') as f:
-            reader = csv.DictReader(f)    
-            # print(i)
-            for row in reader:
-                if i == row['Alpha'].lower():
-                    morse.append(row['Morse'])
-    else:
-        morse.append(' | ')   
+s = input('Your Input: ').lower()
 
-print(f'{ s } : \n{ " ".join(morse) }')
+def morse(s):
+    morse = []
+    for i in s:
+        if i.isalnum():
+            with open('morse.csv') as f:
+                reader = csv.DictReader(f)    
+                # print(i)
+                for row in reader:
+                    if i == row['Alpha'].lower():
+                        morse.append(row['Morse'])
+        else:
+            morse.append(' | ')   
+    return morse
+
+if __name__ == '__main__':
+    print(f'{ s } : \n{ " ".join(morse(s)) }')
